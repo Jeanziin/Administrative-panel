a82df9c2f893110e8cc0ecfd9d6fbb4d047404dd
Jeanziin <jjeanfreitass@gmail.com>
2023-04-20 20:37:51 -0300

    finished

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const connection = require('./database/database');
+
 const categoriesController = require('./categories/CategoriesController');
 const articlesController = require('./articles/ArticlesController');
+const usersController = require("./users/UsersController");
+
 const Article = require('./articles/Article');
 const Category = require('./categories/category');
+const User = require('./users/User');
 
 
 // view engine configuration 
 app.set('view engine', 'ejs');
+
+//Session configuration
+
+app.use(session({
+    secret: "corinthiansMaiorDoBrasil", cookie: {maxAge: 30000}
+}));
 
 // Static files configuration
 app.use(express.static('public'));
@@ -28,11 +39,70 @@ connection
 });
 
 app.use('/', categoriesController); 
-app.use('/', articlesController)
+app.use('/', articlesController);
+app.use('/', usersController);
 
-app.get('/', (req, res) => {
-   res.render("index");
+app.get('/session', (req, res) => {
+
 });
+
+app.get('/leitura', (req, res) => {
+    
+});
+
+app.get("/", (req, res) => {
+    Article.findAll({
+        order:[
+            ['id','DESC']
+        ],
+        limit: 4
+    }).then(articles => {
+        Category.findAll().then(categories => {
+            res.render("index", {articles: articles, categories: categories});
+        });
+    });
+})
+
+app.get("/:slug",(req, res) => {
+    let slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("article", {article: article, categories: categories});
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch( err => {
+        res.redirect("/");
+    });
+})
+  
+
+app.get("/category/:slug",(req, res) => {
+    let slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then( category => {
+        if(category != undefined){
+            Category.findAll().then(categories => {
+                res.render("index",{articles: category.articles,categories: categories});
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch( err => {
+        res.redirect("/");
+    })
+})
+
 
 app.listen(8080, () =>{
     console.log('Server is running on port 8080');
